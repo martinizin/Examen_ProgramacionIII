158bec0aab27706c3ccb671d1011ff12d687bb3c
RJManosalvas1 <roberto.manosalvas@udla.edu.ec>
2024-10-30 17:03:53 -0500

    Clase Guerrero

--- src/ColaGuerreros.java
+++ src/ColaGuerreros.java
@@ -1,3 +1,3 @@
-public class ColaGuerreros {
+public class ColaGuerreros includes Guerrero {
 
 }
--- src/Guerrero.java
+++ src/Guerrero.java
@@ -1,2 +1,24 @@
 public class Guerrero {
+    private String nombre;
+    private String raza;
+    private int nivelPoder;
+    private String habilidad;
+
+    public Guerrero(String nombre, String raza, int nivelPoder, String habilidad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.nivelPoder = nivelPoder;
+        this.habilidad = habilidad;
+    }
+
+    public String mostrarInfo() {
+        return "Nombre: " + nombre + "\n" +
+                "Raza: " + raza + "\n" +
+                "Nivel de Poder: " + nivelPoder + "\n" +
+                "Habilidad: " + habilidad;
+    }
+
+
+
 }
+
