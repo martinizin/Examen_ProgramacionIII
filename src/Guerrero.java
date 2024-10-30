ca8bb89afb120a7ee07f544b0bc9ddf8f7416150
RJManosalvas1 <roberto.manosalvas@udla.edu.ec>
2024-10-30 17:31:16 -0500

    final Clase Guerrero

--- src/Guerrero.java
+++ src/Guerrero.java
@@ -4,6 +4,13 @@ public class Guerrero {
     private int nivelPoder;
     private String habilidad;
 
+    public Guerrero() {
+        this.nombre = "Goku";
+        this.raza = "Saiyajin";
+        this.nivelPoder = 924;
+        this.habilidad = "kamehameha";
+    }
+
     public Guerrero(String nombre, String raza, int nivelPoder, String habilidad) {
         this.nombre = nombre;
         this.raza = raza;
@@ -18,7 +25,37 @@ public class Guerrero {
                 "Habilidad: " + habilidad;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public String getRaza() {
+        return raza;
+    }
 
+    public int getNivelPoder() {
+        return nivelPoder;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public int calculoPoder() {
+        double resultadoNivelPoder;
+
+        if (raza.equals("Saiyajin")) {
+            resultadoNivelPoder = nivelPoder * 1.5;
+            return (int) resultadoNivelPoder;
+        } else if (raza.equals("Namekusein")) {
+            resultadoNivelPoder = nivelPoder * 1.2;
+            return (int) resultadoNivelPoder;
+        } else if (raza.equals("Terricola")) {
+            resultadoNivelPoder = nivelPoder * 1;
+            return (int) resultadoNivelPoder;
+        } else {
+            System.out.println("Raza no encontrada");
+            return -1;
+        }
+    }
 }
-
