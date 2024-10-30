041ab8121817a7ec6fa95e7bbb376c09da463803
Eduardo Andrade <ejac09102006@gmail.com>
2024-10-30 17:34:38 -0500

    GUIForm creado

---
+++ src/examenGUI.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class examenGUI {
+    private JComboBox razaC;
+    private JTextArea textArea1;
+    private JButton agregarGuerreroButton;
+    private JButton mostrarGuerreroButton;
+    private JTextField nombreF;
+    private JTextField nivelP;
+    private JTextField habilidadF;
+
+    public examenGUI() {
+        agregarGuerreroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        mostrarGuerreroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
